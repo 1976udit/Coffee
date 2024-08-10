02a7719ce09d0d0a293b443c77f1be15cd2d9734
1976udit <165473837+1976udit@users.noreply.github.com>
2024-08-10 10:21:29 +0000

    addition

--- Shop/src/components/AppBanner/AppBanner.jsx
+++ Shop/src/components/AppBanner/AppBanner.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import banner from "../../assets/coffee-cover.jpg"
 import {motion} from "framer-motion"
+import anima1 from "../../assets/animaCup.png"
+
+
+
 
 const AppBanner = () => {
     const BannerStyle = {
@@ -15,10 +19,18 @@ const AppBanner = () => {
     <div className='container my-14'>
       <div style={BannerStyle} className='sm:min-h-[400px] sm:flex sm:justify-end sm:items-center rounded-xl'>
         <div>
-            <div className='space-y-6 max-w-xl mx-auto'>
-                <h1 className='text-2xl text-center sm:text-4xl font-semibold'>Live your Life</h1>
+            <div className='space-y-4 max-w-xl mx-auto'>
+                <h1 className='text-2xl mt-5 text-center sm:text-4xl font-semibold'>Live your Life</h1>
                 <p className='text-center sm:px-20'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam, vero!</p>
-                <span>(TODO)</span>
+                <motion.img
+                 animate={{rotate : [0,360]}}
+                 transition={{
+                  duration : 8,
+                  repeat : Infinity,
+                  ease: "linear",
+                  repeatType : 'loop'
+                 }}
+                src={anima1} alt="" className='flex items-center max-h-[400px]' />
             </div>
         </div>
       </div>
